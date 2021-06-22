0489c102c414d2bded05ad1a7cc3f704df6dc7bb
sachusharma233@gmail.com <sachusharma233@gmail.com>
2021-06-21 20:19:50 -0700

    jquery file added

---
+++ assets/js/active-class.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+$(".nav-link").click(function () {
+    $(".nav-link").removeClass("active");
+    // $(".tab").addClass("active"); // instead of this do the below 
+    $(this).addClass("active");   
+});
+});
